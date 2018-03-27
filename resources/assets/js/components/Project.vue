614eb9b5083311e6378f1e25b65541f15875c900
YONGKY ALI <yongkyliee@outlook.com>
2018-03-27 20:52:44 +0700

    added projects page

--- resources/assets/js/components/Project.vue
+++ resources/assets/js/components/Project.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="container">
     <div class="row full-height justify-content-center align-items-center">
-      <div class="row text-center-sm">
-        <div class="col-12">
-          <h1>This page is currently under development 🙏</h1>
-        </div>
-        <div class="col-12">
+      <div class="row text-center-xs text-left-sm">
+        <div class="col-12" style="margin: 150px 0">
+          <h5>Executive Mafia</h5>
+          <a href="https://executivemafia.com" target="_blank">www.executivemafia.com</a>
           <p>
-            But no worries! It's coming real soon! 😄
+            Executive Mafia website, build with PHP Laravel Framework.<br/>
+            Hosted in Virtual Private Server, Amazon S3 Storage, and Mailgun ESP for E-Mail Services.
           </p>
-        </div>
-        <div class="col-12">
+
+          <h5>Executive Media</h5>
+          <a href="https://executivemafia.com" target="_blank">www.executivemedia.group</a>
+          <p>
+            Company profile for PT. Eksekutif Media Utama, build with PHP Laravel Framework.
+          </p>
+
+          <h5>Jasmine Telegram Bot</h5>
+          <a href="https://t.me/jesmin_bot" target="_blank">Jasmine Telegram</a>
+          <p>
+            Jasmine is a bot created as a personal assistant for Executive Mafia.<br/>
+            Jasmine assigned as operator to do complex tasks such as doing rounds and work with Instagram API.<br/>
+            Jasmine was build with PHP Laravel framework.
+          </p>
+
+          <h5>This website.</h5>
+          <a href="https://yongky.xyz" target="_blank">www.yongky.xyz</a>
+          <p>
+            This website is made by myself, using  PHP Laravel as server-side and Vue.JS as front-end.<br/>
+            This website intentionaly made for educational purpose. Laravel will be changed to Python Django in the near future.<br/>
+            Laravel version for this website is open sourced, you can fork it <a href="https://github.com/yongkylie/personal-site" target="_blank">here</a> and make it yours. :)
+          </p>
+
           <router-link :to="{ name: 'landing' }">Go Back </router-link>
         </div>
       </div>
@@ -25,3 +46,9 @@
     } 
   }
 </script>
+
+<style>
+.row {
+  margin: 0 !important;
+}
+</style>
